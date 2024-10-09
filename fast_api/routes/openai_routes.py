77dda0160f6d407aed5ac12b5011ffacfd1c4af5
Pragnesh Anekal <72695068+Pragnesh260@users.noreply.github.com>
2024-10-08 21:45:29 -0400

    Handle larger files

--- fast_api/routes/openai_routes.py
+++ fast_api/routes/openai_routes.py
@@ -3,11 +3,11 @@ from fast_api.schemas.request_schemas import OpenAIRequest
 from fast_api.services.auth_service import get_current_user
 from fast_api.services.openai_service import OpenAIClient
 from project_logging import logging_module
-from typing import Dict
+from typing import Dict, Optional
 
 router = APIRouter()
 
-@router.get("/fetch-openai-response/", response_model=str)
+@router.get("/fetch-openai-response/", response_model=Optional[str])
 def get_openai_response(request: OpenAIRequest, current_user: Dict = Depends(get_current_user)):
     
     # Log the user who is making the request
@@ -16,9 +16,14 @@ def get_openai_response(request: OpenAIRequest, current_user: Dict = Depends(get
     question_selected = request.question_selected
     model = request.model
     annotated_steps = request.annotated_steps
+    file_extract = request.file_extract
+    loaded_file = request.loaded_file
     
     client = OpenAIClient()
 
-    response = client.validation_prompt(question_selected, model, annotated_steps)
+    if file_extract and loaded_file:
+        response = client.file_validation_prompt(loaded_file["path"], question_selected, model)
+    else:
+        response = client.validation_prompt(question_selected, model, annotated_steps)
 
     return response
--- fast_api/services/openai_service.py
+++ fast_api/services/openai_service.py
@@ -28,6 +28,9 @@ and follows the Annotator Steps and provides the answer in the mentioned Output 
 
         self.output_format = "Provide a clear and conclusive answer to the Question being asked. Do not provide any \
 reasoning or references for your answer."
+
+        self.assistant_instruction = """You are an assistant that answers any questions relevant to the \
+file that is uploaded in the thread. """
     
     def format_content(self, question: str, annotator_steps: str = None) -> str:
         if annotator_steps is None:
@@ -87,3 +90,85 @@ reasoning or references for your answer."
         except Exception as e:
             logging_module.log_error(f"An unexpected error occurred: {str(e)}")
             return f"Error-BDIA: {e}"
+        
+    def file_validation_prompt(self, file_path: str, question: str, model: str) -> str:
+        user_content = self.format_content(question)
+        system_content = self.val_system_content
+        try:
+
+            logging_module.log_success(f"System Content: {system_content}")
+            logging_module.log_success(f"User Content: {user_content}")
+
+            file_assistant = self.client.beta.assistants.create(
+                instructions=self.assistant_instruction + system_content,
+                model=model.lower(),
+                tools=[{"type": "file_search"}],
+            )
+
+            logging_module.log_success(f"Assistant created with ID: {file_assistant.id}")
+
+            query_file = self.client.files.create(file=open(file_path, "rb"), purpose="assistants")
+
+            logging_module.log_success(f"File stored with ID: {query_file.id}")
+
+            empty_thread = self.client.beta.threads.create()
+
+            logging_module.log_success(f"Thread created with ID: {query_file.id}")
+
+            self.client.beta.threads.messages.create(
+                empty_thread.id,
+                role="user",
+                content=user_content,
+                attachments=[{"file_id": query_file.id, "tools": [{"type": "file_search"}]}]
+            )
+
+            logging_module.log_success(f"Message created in thread {empty_thread.id} with file {query_file.id}")
+
+            run = self.client.beta.threads.runs.create_and_poll(
+                thread_id=empty_thread.id,
+                assistant_id=file_assistant.id,
+                max_prompt_tokens=30000
+            )
+
+            logging_module.log_success(f"Run executed with ID: {run.id}")
+
+            if run.status == 'completed':
+                messages = self.client.beta.threads.messages.list(
+                    thread_id=run.thread_id
+                )
+
+                logging_module.log_success(f"Response: {messages.data[0].content[0].text.value}")
+
+                self.cleanup_resources(file_assistant.id, query_file.id, empty_thread.id)
+
+                return messages.data[0].content[0].text.value
+            else:
+                logging_module.log_error(f"Run Status: {run.status}")
+                logging_module.log_error(f"Run Status: {run.last_error}")
+                return None
+            
+        except openai.BadRequestError as e:
+            logging_module.log_error(f"Error: {e}")
+            return f"Error-BDIA: {e}"
+        except openai.APIError as e:
+            logging_module.log_error(f"Error: {e}")
+            return f"Error-BDIA: {e}"
+        except Exception as e:
+            logging_module.log_error(f"An unexpected error occurred: {str(e)}")
+            return f"Error-BDIA: {e}"
+    
+    def cleanup_resources(self, assistant_id: str, file_id: str, thread_id: str) -> None:
+        try:
+            # Delete the assistant
+            self.client.beta.assistants.delete(assistant_id)
+            logging_module.log_success(f"Assistant with {assistant_id} deleted successfully")
+
+            # Delete the file
+            self.client.files.delete(file_id)
+            logging_module.log_success(f"Assistant with {file_id} deleted successfully")
+
+            # Delete the thread
+            self.client.beta.threads.delete(thread_id)
+            logging_module.log_success(f"Assistant with {thread_id} deleted successfully")
+        except Exception as e:
+            logging_module.log_error(f"Error occurred while cleaning up resources!")
